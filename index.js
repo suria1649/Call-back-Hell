37d1323d1c244f920fe2ff0b6c5d9465880d08e6
suria1649 <suria1649@gmail.com>
2023-08-08 16:53:56 +0530

    Add HTML and JavaScript code

---
+++ index.js
@@ -0,0 +1,45 @@
+let h1 = document.createElement('h1'); 
+document.body.append(h1);
+setTimeout(()=>{
+  h1.innerHTML="10"
+
+  setTimeout(()=>{
+    h1.innerHTML="9"
+
+    setTimeout(()=>{
+      h1.innerHTML="8"    
+      
+      setTimeout(()=>{
+        h1.innerHTML="7"    
+        
+        setTimeout(()=>{
+          h1.innerHTML="6"
+          
+          setTimeout(()=>{
+            h1.innerHTML="5"
+            
+            setTimeout(()=>{
+              h1.innerHTML="4"  
+
+              setTimeout(()=>{
+                h1.innerHTML="3"  
+
+                setTimeout(()=>{
+                  h1.innerHTML="2"  
+
+                  setTimeout(()=>{
+                    h1.innerHTML="1"  
+
+                    setTimeout(()=>{
+                      h1.innerHTML="Happy Independence Day"
+                      },1000)
+                    },1000)
+                  },1000)
+                },1000)
+              },1000)                      
+          },1000)
+        },1000)  
+      },1000)
+    },1000)
+  },1000)
+},1000);
